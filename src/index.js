e8433666b961055df83315fda792996ea0bc9688
maxifjaved <axif.javed@gmail.com>
2017-12-21 17:26:17 +0500

    es6 configured + babelrc added

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+
+const typeDefs = [`
+type Query {
+  hello: String
+}
+ 
+schema {
+  query: Query
+}`]
+
+const resolvers = {
+  Query: {
+    hello(root) {
+      return 'world'
+    }
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers})
+const app = express()
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema}))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'))
