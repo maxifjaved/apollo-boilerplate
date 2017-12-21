4cc9abbac048921d342016645b206130f7ac99be
maxifjaved <axif.javed@gmail.com>
2017-12-21 17:35:54 +0500

    standeredjs linting added

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ schema {
 
 const resolvers = {
   Query: {
-    hello(root) {
+    hello (root) {
       return 'world'
     }
   }
 }
 
-const schema = makeExecutableSchema({ typeDefs, resolvers})
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 const app = express()
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema}))
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'))
